e70024f2ef012b75a44bc547dfa8e3dc672e36bf
Saeraphinx <37343661+Saeraphinx@users.noreply.github.com>
2022-05-30 12:44:54 -0500

    added multiple sources, rework a bunch of other stuff

--- NyaDesktop/Form1.Designer.cs
+++ NyaDesktop/Form1.Designer.cs
@@ -31,32 +31,23 @@ namespace NyaDesktop
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.sfw_comboBox = new System.Windows.Forms.ComboBox();
-            this.nsfw_comboBox = new System.Windows.Forms.ComboBox();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.SettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setSaveDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.endpointToolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
+            this.SFWToolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.serviceToolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.Location = new System.Drawing.Point(3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 21);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Save Path";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -81,78 +72,6 @@ namespace NyaDesktop
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // comboBox1
-            // 
-            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "sfw",
-            "nsfw"});
-            this.comboBox1.Location = new System.Drawing.Point(246, 2);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(75, 21);
-            this.comboBox1.TabIndex = 5;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
-            // sfw_comboBox
-            // 
-            this.sfw_comboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.sfw_comboBox.Enabled = false;
-            this.sfw_comboBox.FormattingEnabled = true;
-            this.sfw_comboBox.Items.AddRange(new object[] {
-            "waifu",
-            "neko",
-            "shinobu",
-            "megumin",
-            "bully",
-            "cuddle",
-            "cry",
-            "hug",
-            "awoo",
-            "kiss",
-            "lick",
-            "pat",
-            "smug",
-            "bonk",
-            "yeet",
-            "blush",
-            "smile",
-            "wave",
-            "highfive",
-            "handhold",
-            "nom",
-            "bite",
-            "glomp",
-            "slap",
-            "kill",
-            "kick",
-            "happy",
-            "wink",
-            "poke",
-            "dance",
-            "cringe"});
-            this.sfw_comboBox.Location = new System.Drawing.Point(328, 2);
-            this.sfw_comboBox.Name = "sfw_comboBox";
-            this.sfw_comboBox.Size = new System.Drawing.Size(74, 21);
-            this.sfw_comboBox.TabIndex = 6;
-            this.sfw_comboBox.Visible = false;
-            // 
-            // nsfw_comboBox
-            // 
-            this.nsfw_comboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.nsfw_comboBox.Enabled = false;
-            this.nsfw_comboBox.FormattingEnabled = true;
-            this.nsfw_comboBox.Items.AddRange(new object[] {
-            "waifu",
-            "neko",
-            "trap",
-            "blowjob"});
-            this.nsfw_comboBox.Location = new System.Drawing.Point(328, 2);
-            this.nsfw_comboBox.Name = "nsfw_comboBox";
-            this.nsfw_comboBox.Size = new System.Drawing.Size(74, 21);
-            this.nsfw_comboBox.TabIndex = 7;
-            this.nsfw_comboBox.Visible = false;
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -160,7 +79,7 @@ namespace NyaDesktop
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Location = new System.Drawing.Point(12, 13);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(401, 283);
+            this.pictureBox1.Size = new System.Drawing.Size(401, 287);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
@@ -168,13 +87,9 @@ namespace NyaDesktop
             // panel1
             // 
             this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.sfw_comboBox);
-            this.panel1.Controls.Add(this.nsfw_comboBox);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.comboBox1);
-            this.panel1.Location = new System.Drawing.Point(12, 298);
+            this.panel1.Location = new System.Drawing.Point(12, 302);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(402, 23);
             this.panel1.TabIndex = 9;
@@ -194,38 +109,107 @@ namespace NyaDesktop
             this.contextMenuStrip1.ShowImageMargin = false;
             this.contextMenuStrip1.Size = new System.Drawing.Size(36, 4);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Black;
+            this.menuStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SettingsToolStripMenuItem,
+            this.endpointToolStripComboBox2,
+            this.SFWToolStripComboBox1,
+            this.serviceToolStripComboBox1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 328);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(426, 27);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // SettingsToolStripMenuItem
+            // 
+            this.SettingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setSaveDirectoryToolStripMenuItem});
+            this.SettingsToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem";
+            this.SettingsToolStripMenuItem.Size = new System.Drawing.Size(61, 23);
+            this.SettingsToolStripMenuItem.Text = "Settings";
+            // 
+            // setSaveDirectoryToolStripMenuItem
+            // 
+            this.setSaveDirectoryToolStripMenuItem.Name = "setSaveDirectoryToolStripMenuItem";
+            this.setSaveDirectoryToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.setSaveDirectoryToolStripMenuItem.Text = "Set Save Directory";
+            this.setSaveDirectoryToolStripMenuItem.Click += new System.EventHandler(this.setSaveDirectoryToolStripMenuItem_Click);
+            // 
+            // endpointToolStripComboBox2
+            // 
+            this.endpointToolStripComboBox2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.endpointToolStripComboBox2.BackColor = System.Drawing.Color.Black;
+            this.endpointToolStripComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.endpointToolStripComboBox2.ForeColor = System.Drawing.Color.White;
+            this.endpointToolStripComboBox2.Name = "endpointToolStripComboBox2";
+            this.endpointToolStripComboBox2.Size = new System.Drawing.Size(75, 23);
+            // 
+            // SFWToolStripComboBox1
+            // 
+            this.SFWToolStripComboBox1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.SFWToolStripComboBox1.BackColor = System.Drawing.Color.Black;
+            this.SFWToolStripComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SFWToolStripComboBox1.ForeColor = System.Drawing.Color.White;
+            this.SFWToolStripComboBox1.Items.AddRange(new object[] {
+            "sfw",
+            "nsfw"});
+            this.SFWToolStripComboBox1.Name = "SFWToolStripComboBox1";
+            this.SFWToolStripComboBox1.Size = new System.Drawing.Size(75, 23);
+            // 
+            // serviceToolStripComboBox1
+            // 
+            this.serviceToolStripComboBox1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.serviceToolStripComboBox1.BackColor = System.Drawing.Color.Black;
+            this.serviceToolStripComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.serviceToolStripComboBox1.ForeColor = System.Drawing.Color.White;
+            this.serviceToolStripComboBox1.Name = "serviceToolStripComboBox1";
+            this.serviceToolStripComboBox1.Size = new System.Drawing.Size(100, 23);
+            this.serviceToolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.serviceToolStripComboBox1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(426, 333);
+            this.ClientSize = new System.Drawing.Size(426, 355);
+            this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.MinimumSize = new System.Drawing.Size(442, 372);
             this.Name = "Form1";
             this.Text = "Nya";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ProgramCloser);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox sfw_comboBox;
-        private System.Windows.Forms.ComboBox nsfw_comboBox;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem SettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setSaveDirectoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox endpointToolStripComboBox2;
+        private System.Windows.Forms.ToolStripComboBox SFWToolStripComboBox1;
+        private System.Windows.Forms.ToolStripComboBox serviceToolStripComboBox1;
     }
 }
 
---
+++ NyaDesktop/NyaAPIManager.cs
@@ -0,0 +1,36 @@
+﻿using DiscordRPC;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NyaDesktop
+{
+    static class NyaAPIManager
+    {
+        public static DiscordRpcClient DiscordClient { get; private set; }
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            startRichPrecense();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(DiscordClient));
+        }
+
+        public static void startRichPrecense()
+        {
+            DiscordClient = new DiscordRpcClient("901892124679741541");  //Creates the client
+        }
+
+        public static void ProgramCloser()
+        {
+            DiscordClient.Dispose();
+        }
+    }
+}
